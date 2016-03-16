cc235f5449f90a87df054d5fecfbf79c2044163d
Wojciech Michałowski <wojciech.bwa@gmail.com>
2016-03-16 18:18:12 +0100

    Generowanie permutacji - commit.

--- permutacje.cpp
+++ permutacje.cpp
@@ -2,36 +2,53 @@
 #include "zad4/klasa_kolejka.hpp"
 using namespace std;
 
-void Permutacje(string ciag_znakow, kolejka& permutacje, string permutacja="") {
+// Przy pierwszym wywolaniu funkcji n to dlugosc ciagu znakow
+void Permutacje(string &ciag_znakow, unsigned int n, kolejka& permutacje) {
 
-  if (ciag_znakow.size() > 0) {
+  if (n == 1) {
 
-    for (unsigned int i = 0; i < ciag_znakow.size(); i++) {
-
-      char znak_chr[1] {ciag_znakow[0]};
-      string znak(znak_chr);
-      string tymczasowy = ciag_znakow;
-      tymczasowy.erase(i, 1);
-      Permutacje(tymczasowy, permutacje, permutacja + znak);
-
-    }
-  }
-  
-  else {
-
-    element* elem = new element(permutacja);
+    element* elem = new element(ciag_znakow);
     permutacje.dodaj(elem);
     
-  } 
+  }
+
+  else {
+
+    for (unsigned int i = 0; i < n-1; i++) {
+
+      Permutacje(ciag_znakow, n-1, permutacje);
+      
+      if (n % 2 == 0) {
+
+        string znak = ciag_znakow.substr(n-1, 1);
+	ciag_znakow.replace(n-1, 1, ciag_znakow.substr(i, 1));
+	ciag_znakow.replace(i, 1, znak);
+	
+      }
+      
+      else {
+	string znak = ciag_znakow.substr(n-1, 1);
+	ciag_znakow.replace(n-1, 1, ciag_znakow.substr(0, 1));
+	ciag_znakow.replace(0, 1, znak);
+      }
+    }
+    
+    Permutacje(ciag_znakow, n-1, permutacje);
+  }
 }
 
 int main() {
 
-  string ciag("abcd");
+  string ciag;
+
+  cout << "Ciag znakow: ";
+  cin >> ciag;
+  
   kolejka permutacje;
+  kolejka palindromy;
   element* elem;
   
-  Permutacje(ciag, permutacje);
+  Permutacje(ciag, ciag.size(), permutacje);
 
   while (true) {
 
